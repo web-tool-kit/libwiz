f909ecf25db95f2fb1b6ef348f6171918ebd3bfe
xettri <imbharatrawat@gmail.com>
2024-10-24 14:25:37 +0545

    Avoid use babel config file

--- src/transpiler/babel.ts
+++ src/transpiler/babel.ts
@@ -59,6 +59,7 @@ export async function transpileAsync(props: BuildProps, sourceFiles: string[]) {
               sourceMaps: Boolean(moduleConfig?.output?.sourceMap),
               comments: Boolean(moduleConfig?.output?.comments),
               ...babelConfig,
+              configFile: false,
             },
           );
 
